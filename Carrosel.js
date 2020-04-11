faa3ae2dfb6df2acd910ee9c4fdc5716ab0ce2ed
renansalvino <renansalvinomartins454@gmail.com>
2020-04-11 18:03:17 -0300

    [CREATE] Slides, [CREATE] Menu de Navegação

---
+++ Carrosel.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+var speed = 5000;
+var run = setInterval('roatate()',speed);
+
+
+var item_width = $('#itens li').outerWidth();
+var left_value = item_width * (-1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
